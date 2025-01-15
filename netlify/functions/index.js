0a277edd4f82515d44406004c0dfb629a3de9a82
Tamas Piros <2042718+tpiros@users.noreply.github.com>
2025-01-15 13:29:45 +0800

    updates

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -1,6 +1,3 @@
-import app from './server.js';
-import imageServer from '../../server/image-server.js';
-
 // app.listen(3000, () => {
 //   console.log('Up and running on http://localhost:3000/');
 // });
